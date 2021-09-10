33023c9abf1033db3eb0fee631b4d9e590d8e3b6
Jordan Dichev <jordan.dichev@gmail.com>
2021-09-10 21:40:40 +0300

    Small improvements to script

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 const fs = require("fs");
+const path = require("path");
 const axios = require("axios");
 
 require("dotenv").config();
 
 const headersList = {
   Accept: "application/vnd.github.cloak-preview",
-  "User-Agent": "Thunder Client (https://www.thunderclient.io)",
 };
 
+const delimiter = ";;";
+const github = process.env.GH_URL;
 const members = process.env.USERS.split(",");
-console.log(members);
-
 const repos = process.env.REPOS.split(",");
-console.log(repos);
-
 const startDate = process.env.START;
-console.log(startDate);
-
 const endDate = process.env.END;
-console.log(endDate);
+const outDir = path.join(__dirname, "out");
+
+if (!fs.existsSync(outDir)) {
+  fs.mkdirSync(outDir);
+}
 
 const doRequest = async (member, repo) => {
   const reqOptionsCommits = {
     url:
-      `https://api.github.com/search/commits?q=author:${member}+repo:${repo}` +
-      `+author-date:${startDate}..${endDate}+merge:false`,
+      `${github}/search/commits?q=author:${member}+repo:${repo}` +
+      `+committer-date:${startDate}..${endDate}+merge:false`,
     method: "GET",
     headers: headersList,
     auth: {
@@ -40,35 +40,66 @@ const doRequest = async (member, repo) => {
     .catch((error) => console.log(error));
 
   csvRows.push(
-    `${member},${repo},${response.data.total_count},start ${startDate}, end ${endDate}`
+    [
+      `${member}`,
+      `${repo}`,
+      `${response.data.total_count}`,
+      `start ${startDate}`,
+      `end$ {endDate}`,
+    ].join(delimiter)
   );
 
   response.data.items.forEach((item) => {
-    csvRows.push(`${item.commit.message.split("\n")[0]},${item.html_url}`);
+    csvRows.push(
+      [`${item.commit.message.split("\n")[0]}`, `${item.html_url}`].join(
+        delimiter
+      )
+    );
   });
 
   const resultString = csvRows.join("\n");
 
-  return resultString;
+  return { resultString, totalCount: response.data.total_count };
 };
 
 const main = async () => {
+  let totalCount = 0;
+  const totalPerRepo = new Map();
+
   for (let member of members) {
     const resultStrings = [];
 
     for (let repo of repos) {
-      const resultString = await doRequest(member, repo);
+      if (!totalPerRepo.has(repo)) {
+        totalPerRepo.set(repo, 0);
+      }
 
-      resultStrings.push(resultString);
+      const res = await doRequest(member, repo);
+
+      resultStrings.push(res.resultString);
+      totalCount += res.totalCount;
+      totalPerRepo.set(repo, totalPerRepo.get(repo) + res.totalCount);
     }
 
     fs.writeFileSync(
-      `./out/${member}.csv`,
+      path.join(__dirname, "out", `${member}.csv`),
       resultStrings.join(`\n${"-".repeat(80)}\n`)
     );
 
     console.log(`${member} saved`);
   }
+
+  const sumsResultStrings = [];
+  sumsResultStrings.push(`TOTAL${delimiter}${totalCount}`);
+
+  totalPerRepo.forEach((value, key) => {
+    sumsResultStrings.push(`${key}${delimiter}${value}`);
+  });
+
+  fs.writeFileSync(
+    path.join(__dirname, "out", "summary.csv"),
+    sumsResultStrings.join(`\n${"-".repeat(80)}\n`)
+  );
 };
 
 main();
